2c57b6d33070781ceea84746fc6aba81bb1daea4
Gustavo Grecco <gustavo.grecco@hotmail.com>
2021-10-14 15:58:55 -0300

    inicio aulas array

---
+++ aulas-array/src/teste/aulaArray.java
@@ -0,0 +1,16 @@
+package teste;
+
+public class aulaArray {
+
+	public static void main(String[] args) {
+
+		int[] idades = new int[5];
+		
+		for(int i = 0; i < idades.length; i++) {
+			idades[i] = i + 10 ;
+			System.out.println(idades[i]);
+		}
+
+	}
+
+}
